1807e6a70bc67ed48e7e45ebc9db115e07e74e0a
Arthur S. Nascimento <arthur.soares.nascimento31@gmail.com>
2023-09-28 13:11:35 -0300

    Subindo projetos

--- forms/solicitacao_compra_arthur/events/displayFields.js
+++ forms/solicitacao_compra_arthur/events/displayFields.js
@@ -1,6 +1,7 @@
 function displayFields(form,customHTML){ 
+    //log.info("iniciando display fields")
     var nNumStateValue = getValue("WKNumState");
 
-    form.setValue("etapa", nNumStateValue)
+    form.setValue("etapa", nNumStateValue);
     
 }
--- forms/solicitacao_compra_arthur/script.js
+++ forms/solicitacao_compra_arthur/script.js
@@ -10,40 +10,42 @@ $(function(){
         $("#gestor").show();
     }   
 })
-$("#aprovarGestorSim").on("click", function () {  
-    let content = ` 
-    <div style="text-align: center;">
-        <img src="icons/warning.png" alt="" style="width: 100px;">
-        <h3>Tem certeza que deseja aprovar?</h3>
-        <div>
-            <input type="button" id="sim" name="sim" value="sim" onclick="aprovarGestor(this)">
-            <input type="button" id="nao" name="nao" value="não" onclick="aprovarGestor(this)">
-        </div>
-    </div>
-`
-    let myModal = FLUIGC.modal({
-        title: 'Confirmar',
-        content: content,
-        id: 'confirmar-modal'
-    },
-        function (err, data) {
-            if (err) {
-               
-            } else {
-                // fazer algo com os dados(data)
-            }
-        }
-    )
-})
-$(function aprovarGestor(botao){
-    var resposta = $(botao).val(); 
-
-    console.log(resposta)
-    if(resposta == "sim"){
-        $("#aprovaGestor").val("sim");
-    }else if(resposta == "não"){
-        $("#aprovaGestor").val("não");
+function escolhaGestor(escolha){
+    var escolhaAtual;
+    if(escolha !== escolhaAtual){
+        $("#aprovaGestor").val(escolha);
+        escolhaAtual = escolha;
     }
+    
+}
+$(document).ready(() => {
+    $("#aprovarGestorSim").on("click", function () {  
+        let content = ` 
+        <div style="text-align: center;">
+            <img src="icons/warning.png" alt="" style="width: 100px;">
+            <h3>Tem certeza que deseja aprovar?</h3>
+            <div>
+                <input type="button" id="sim" name="sim" onclick="escolhaGestor('Sim')">
+                <input type="button" id="nao" name="nao" onclick="escolhaGestor('Não')">
+            </div>
+        </div>
+    `
+        let myModal = FLUIGC.modal({
+            title: 'Confirmar',
+            content: content,
+            id: 'confirmar-modal'
+        },
+            function (err, data) {
+                if (err) {
+                   
+                } else {
+                    // fazer algo com os dados(data)
+                }
+            }
+        )
+    })
+
 })
+
 
 
